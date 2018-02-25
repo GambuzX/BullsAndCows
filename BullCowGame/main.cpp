3cf6f9056eda0fdb8708f0688afe7108998efe53
Mário Gil <mg_mesquita@hotmail.com>
2018-02-25 20:23:55 +0000

    Improved code readability

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -4,40 +4,46 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
 
 //The entry point for our application
 int main()
 {
 	PrintIntro();
 
-	// Loop for the number of turns asking for guesses
-	const int NUMBER_OF_TURNS = 5;
-	for (int i = 1; i <= NUMBER_OF_TURNS; i++)
-		GetGuessAndPrintBack();
+	PlayGame();
 		
 	cout << endl << endl;
 	return 0;
 }
 
-//Introduce the game
+
 void PrintIntro()
 {	
+	//Introduce the game
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n\n";
 	return;
 }
 
-//Get a guess from the player
-string GetGuessAndPrintBack()
+void PlayGame()
 {
+	// Loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int i = 1; i <= NUMBER_OF_TURNS; i++)
+	{
+		string guess = GetGuess();
+		cout << "Your guess was: " << guess << endl << endl;
+	}
+}
+
+string GetGuess()
+{
+	//Get a guess from the player
 	string guess = "";
 	cout << "What's your guess? ";
-	getline(cin, guess);
-
-	//repeat the guess back to the player
-	cout << "Your guess was: " << guess << endl << endl;
-	
+	getline(cin, guess);	
 	return guess;
 }
