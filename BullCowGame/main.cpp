10064ff4247a5ce4e6ea6fd8e75434463baab55d
Mário Gil <mg_mesquita@hotmail.com>
2018-02-25 19:40:01 +0000

    For loop for the number of guesses

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -6,11 +6,15 @@ using namespace std;
 void PrintIntro();
 string GetGuessAndPrintBack();
 
-//the entry point for our application
+//The entry point for our application
 int main()
 {
 	PrintIntro();
-	GetGuessAndPrintBack();
+
+	// Loop for the number of turns asking for guesses
+	const int NUMBER_OF_TURNS = 5;
+	for (int i = 1; i <= NUMBER_OF_TURNS; i++)
+		GetGuessAndPrintBack();
 		
 	cout << endl << endl;
 	return 0;
@@ -25,7 +29,7 @@ void PrintIntro()
 	return;
 }
 
-//get a guess from the player
+//Get a guess from the player
 string GetGuessAndPrintBack()
 {
 	string guess = "";
@@ -33,7 +37,7 @@ string GetGuessAndPrintBack()
 	getline(cin, guess);
 
 	//repeat the guess back to the player
-	cout << "Your guess was: " << guess;
+	cout << "Your guess was: " << guess << endl << endl;
 	
 	return guess;
 }
